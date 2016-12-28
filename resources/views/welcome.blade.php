a453b01c26d580988f78c4bee3f7bcd512bc955a
joelengt <joelengt@gmail.com>
2016-12-28 14:01:28 -0500

    Add Header Style

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1"/>
+
+        <title>Perritos Lindos</title>
+        
+        <!-- Facebook  -->
+        <meta property="og:title" content="Perritos Lindos">
+        <meta property="og:description" content="Compra los mejores perros de raza">
+        <meta property="og:image" content="http://perritoslindos.pe/images/facebook/Cover.png">
+        <meta property="og:url" content="http://perritoslindos.pe">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <link rel="stylesheet" href="./css/header/index.css">
+        <link rel="stylesheet" href="./css/elementos/index.css">
+        <link rel="stylesheet" href="./css/footer/index.css">
+
+    </head>
+    <body>
+        <header class="HeaderMain">
+            <div class="HeaderMain__box">
+                <div class="HeaderMain__logo">
+                    <div class="HeaderMain__logo--img">
+                        <img src="./images/assets/logo_cabecera@2x.png" alt="">
+                    </div>
+                    <div class="HeaderMain__logo--text">
+                        <p>Compra los mejores perros de raza</p>
+                    </div>
+                    <div class="HeaderMain__logo--btn">
+                        <a href="">Conoce a todos</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section class="Perritos">
+            <div class="Perritos__box">
+                <article class="Perritos__item">
+                    <div class="Perritos__item--cover">
+                        <div class="Perritos__item--cover--image">
+                            <img src="./images/assets/dog1-beagle.jpg" alt="">
+                        </div>
+                        <div class="Perritos__item--cover--modal" style="display: none;">
+                            <a href="">Cómprame ya!</a>
+                        </div>
+                    </div>
+                    <div class="Perritos__item--info">
+                        <div class="Perritos__item--info__text">
+                            <strong>Yorkshire Terrier</strong>
+                            <p>4 meses</p>
+                            <h3>Precio S/500.00</h3>
+                        </div>
+                        <div class="Perritos__item--info__plus">
+                            <img src="./images/assets/icon_vermas@2x.png">
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </section> 
+        <footer class="FooterMain">
+            <div class="FooterMain__box">
+                <div class="FooterMain__logo">
+                    <img src="./images/assets/logo_footer@2x.png" alt="">
+                </div>
+                <div class="FooterMain__text">
+                    <p>2016 Perritos Lindos - Todos los derechos reservados</p>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
